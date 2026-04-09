045aed6461b190f2b74edbf1cb663940ab7e0244
k.you <119655633+raymondyeung2022@users.noreply.github.com>
2026-04-09 10:43:10 +0900

    Fix file reference in question6.blade.php

--- SampleApp202406/resources/views/question6.blade.php
+++ SampleApp202406/resources/views/question6.blade.php
@@ -16,7 +16,7 @@
         <h4>javascriptの数字計算問題</h4>
         <p>数字1のテキストボックスと数字2のテキストボックスに、それぞれ数字を入力して、「足し算」ボタンをクリックしても正しく計算できません</p>
         <p>正しく計算できるように修正してください</p>
-        <p>変更するべきファイル: SampleApp202406\resources\views\question.blade.php</p>
+        <p>変更するべきファイル: SampleApp202406\resources\views\question6.blade.php</p>
     </div>
     <div class="row">
         <div class="col-1">数字1: </div>
